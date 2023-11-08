4adaa91a436e8789cb3d88e93b8b6ff07a820c94
Aleksey Kononenko <a.kononenko@neovoxtech.ru>
2023-11-08 21:19:02 +0300

    - Обновил модель - Добавил PostController - Обновил route - Вывел первый пост в dd()

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -5,7 +5,12 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * @property string $title Заголовок поста
+ */
 class Post extends Model
 {
     use HasFactory;
+
+    protected $table = 'posts';
 }
