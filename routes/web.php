7e57579036addf427792568ff74508d3323126aa
Aleksey Kononenko <a.kononenko@neovoxtech.ru>
2023-11-13 20:39:09 +0300

    - Использование middleware

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [\App\Http\Controllers\HomeController::class, 'index']);
 
 Route::group([], function() {
-    Route::get('/posts', IndexController::class)->name('posts.index');
+    Route::get('/posts', IndexController::class)->name('posts.index')->middleware('admin');
     Route::get('/posts/create', \App\Http\Controllers\Post\CreateController::class)->name('posts.create');
     Route::post('/posts', \App\Http\Controllers\Post\StoreController::class)->name('posts.store');
     Route::get('/posts/{post}', \App\Http\Controllers\Post\ShowController::class)->name('posts.show');
@@ -27,3 +25,7 @@ Route::group([], function() {
     Route::patch('/posts/{post}', \App\Http\Controllers\Post\UpdateController::class)->name('posts.update');
     Route::delete('/posts/{post}', \App\Http\Controllers\Post\DestroyController::class)->name('posts.destroy');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
